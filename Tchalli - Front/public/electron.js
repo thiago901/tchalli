9287312cb9ab32987d62eab305dbd20572452618
Thiago Santos <tsrocha901@gmail.com>
2020-11-02 12:50:57 -0300

    babel config

--- Tchalli - Front/public/electron.js
+++ Tchalli - Front/public/electron.js
@@ -9,6 +9,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon:'../src/assets/vestir.png',
     webPreferences: {
       nodeIntegration: true,
     },
